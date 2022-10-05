c58b8ced6ecfc1488766153330e2d40738cbe5a0
willcordeiro <willcordeiro800@gmail.com>
2022-10-05 20:56:34 -0300

    added keyboard inputs

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,14 +2,27 @@ import { useFrame, useThree } from "@react-three/fiber";
 import { useSphere } from "@react-three/cannon";
 import { useEffect, useRef } from "react";
 import { Vector3 } from "three";
+import { useKeyboard } from "../hooks/useKeyboard";
 
 export const Player = () => {
+  const actions = useKeyboard();
+  console.log(
+    "actions",
+    Object.entries(actions).filter(([k, v]) => v)
+  );
   const { camera } = useThree();
   const [ref, api] = useSphere(() => ({
     mass: 1,
     type: "Dynamic",
-    position: [0, 0, 0],
+    position: [0, 1, 0],
   }));
+
+  const vel = useRef([0, 0, 0]);
+  useEffect(() => {
+    api.velocity.subscribe((v) => {
+      vel.current = v;
+    });
+  }, [api.velocity]);
 
   const pos = useRef([0, 0, 0]);
   useEffect(() => {
@@ -21,6 +34,8 @@ export const Player = () => {
     camera.position.copy(
       new Vector3(pos.current[0], pos.current[1], pos.current[2])
     );
+
+    api.velocity.set(0, 1, 0);
   });
 
   return <mesh ref={ref}></mesh>;
